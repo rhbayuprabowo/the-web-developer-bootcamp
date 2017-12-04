e964f4bbb0c896ec5c353ddd9b6cb2d889259c25
R. H. Bayu Prabowo <rh.bayu.prabowo@outlook.com>
2017-12-05 06:49:51 +0700

    fix code to be more efficient

--- exercise-javascript-function/function.js
+++ exercise-javascript-function/function.js
@@ -1,33 +1,16 @@
 function isEven(number) {
-    if (number % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return number % 2 === 0;
 }
 
 function factorial(number) {
-    if (number === 0) {
-        return 1;
-    } else {
-        var result = 1;
-        while (number > 1) {
-            result*=number;
-            number--;
-        }
-        return result;
+    var result = 1;
+    while (number > 1) {
+        result *= number;
+        number--;
     }
+    return result;
 }
 
 function kebabToSnake(teks) {
     return teks.split('-').join('_');
 }
-
-var number = Number(prompt("Input number : "));
-alert("The number : " + number + " is " + isEven(number) +
-      " and the factorial of tha number is " + factorial(number));
-
-var teks = prompt("Input kebab case sentence");
-alert("The snake case of \" " + teks + "\" is \"" +
-        kebabToSnake(teks) + "\"");
-
