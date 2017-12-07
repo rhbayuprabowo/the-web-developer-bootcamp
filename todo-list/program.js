15187f948fbc3897900337ad1054c5fa6b9be394
R. H. Bayu Prabowo <rh.bayu.prabowo@outlook.com>
2017-12-08 00:06:33 +0700

    improve code

--- todo-list/program.js
+++ todo-list/program.js
@@ -6,7 +6,6 @@ while (input!=="quit") {
     //Execute if key "new" is selected
     if (input==="new") {
         addTodos(prompt("Input what do you want todo"));
-        getAllTodos();
     } 
     //Execute if key "list" is selected
     else if (input === "list") {
@@ -16,7 +15,6 @@ while (input!=="quit") {
     else if (input==="delete") {
         var index = Number(prompt("Input index todos is going to delete"));
         deleteTodos(index);
-        getAllTodos();
     }
     //handle again input from user
     input = prompt("What do you want to do ?");
@@ -25,14 +23,13 @@ console.log("You are quit the app!!!");
 
 function addTodos(value) {
     todos.push(value);
+    console.log("You have added todo to list :)")
 }
 
 function getAllTodos() {
     console.log("***************");
-    var i = 0;
-    todos.forEach(element => {
+    odos.forEach(function(element, i) {
         console.log(i +" : "+ element);
-        i++;
     });
     console.log("***************");
 }
@@ -40,6 +37,7 @@ function getAllTodos() {
 function deleteTodos(index) {
     if (index >= 0  && index < todos.length) {
         todos.splice(index,1);
+        console.log("You have deleted a todo!!!")
     }
     else {
         console.log("Index " +index+ " can not be found!");
