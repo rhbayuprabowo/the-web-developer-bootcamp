77444355e3fbd8e3264d91f95075e1a511924f3d
R. H. Bayu Prabowo <rh.bayu.prabowo@outlook.com>
2017-12-09 23:55:25 +0700

    score keeper exercise

--- score-keeper-exercise/program.js
+++ score-keeper-exercise/program.js
@@ -1,35 +1,46 @@
 var maxScoreLabel = document.querySelector('#maxScore');
 var input = document.querySelector('input');
-var buttonsOne = document.querySelector('.playerOne');
-var buttonsTwo = document.querySelector('.playerTwo');
-var buttons = document.querySelectorAll('button');
+var buttonOne = document.querySelector('.playerOne');
+var buttonTwo = document.querySelector('.playerTwo');
+var buttonReset = document.querySelector('.reset');
 var maxGame = 5;
-
-var players = [{
-    playerName: "#playerOne",
-    score: 0
-  },
-  {
-    playerName: "#playerTwo",
-    score: 0
-  }
-];
 maxScoreLabel.textContent = maxGame;
 
-buttons.addEventListener('click', function() {
-  alert('tes');
+var players = [{
+    label: document.querySelector('#playerOne'),
+    score: 0
+}, {
+    label: document.querySelector('#playerTwo'),
+    score: 0
+}];
+
+input.addEventListener('input', function(){
+	maxGame = this.value;
+	maxScoreLabel.textContent = maxGame;
 });
 
-// buttonsOne.addEventListener('click',function() {
-//     if (players[0].score < maxGame){
-//       players[0].score+=1;
-//       document.querySelector(players[0].playerName).textContent = players[0].score;
-//     }
-//   });
-//
-//   buttonsTwo.addEventListener('click',function() {
-//       if (players[1].score < maxGame){
-//         players[1].score+=1;
-//         document.querySelector(players[1].playerName).textContent = players[1].score;
-//       }
-//     });
+buttonOne.addEventListener('click', function() {
+    addScore(0);
+});
+
+buttonTwo.addEventListener('click', function() {
+    addScore(1);
+});
+
+buttonReset.addEventListener('click', function() {
+    players.forEach(function(element) {
+        element.score = 0;
+        element.label.textContent = 0;
+        element.label.style.color = 'black';
+    });
+});
+
+function addScore(playerIndex) {
+    if (players[0].score < maxGame && players[1].score < maxGame) {
+        players[playerIndex].score += 1;
+        players[playerIndex].label.textContent = players[playerIndex].score;
+    }
+    if (players[playerIndex].score >= maxGame) {
+        players[playerIndex].label.style.color = 'green';
+    }
+}
